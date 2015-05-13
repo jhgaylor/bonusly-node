7d6bb7b36343fdfe91453ee532c8515b3e74a94e
Jake Gaylor <jhgaylor@gmail.com>
2015-05-13 16:03:56 -0500

    add general example

---
+++ examples/general.js
@@ -0,0 +1,17 @@
+var Bonusly = require('../main');
+
+// grab an api key from the environment.
+var API_KEY = process.env.BONUSLY_API_KEY;
+
+// create an api client with that key.
+var b = Bonusly(API_KEY);
+
+// Get all the bonuses for the current user (as determined by the api key)
+// Bonusly calls return a promise for the results.
+b.bonuses.getAll({}).then(function (results) {
+  console.log("Got data", results.data);
+})
+// error propogation is done though rejecting promises
+.catch(function (err) {
+	console.log("Experienced an error getting all bonuses.", err.stack);
+});
